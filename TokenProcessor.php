26a502ef74a895128713850de9bbbb5de1703bc2
DanylBeeh <dany151011@gmail.com>
2023-06-07 17:34:51 +0700

    Add files via upload
    fixed some typo

--- TokenProcessor.php
+++ TokenProcessor.php
@@ -1,12 +1,19 @@
 <?php
 class RBAC_TokenProcessor{
     //token settings
-    private $secretAppKey = "superSecretKey- dont sahare to anyone";
-    
+    private $secretAppKey = "superSecretKey - dont share to anyone";
     private $cipherMethod = "aes-128-cbc";
     
-    //magic number for random bytes
+    //magic number for random bytes no need to change
     private $IV = 0x0009999987656789;
+
+    //database setting
+    private $driver="mysql";
+
+
+
+
+    //dont change from here on
     private $DB_host ;
     private $DB_username ;
     private $DB_password;
@@ -16,7 +23,7 @@ class RBAC_TokenProcessor{
     private $passwordCollumn; 
   
     public function connection(){
-        $pdoStr = "mysql:host=".$this->DB_host.";dbname=".$this->DBName.";charset=UTF8";
+        $pdoStr = $this->driver.":host=".$this->DB_host.";dbname=".$this->DBName.";charset=UTF8";
         try{
             $con = new PDO($pdoStr,$this->DB_username,$this->DB_password);
             if($con){
@@ -31,19 +38,17 @@ class RBAC_TokenProcessor{
     //make private after create
     public function RBACgetTokenDetail($token){
         $tokenDecrypted = openssl_decrypt($token,$this->cipherMethod,$this->secretAppKey,0,$this->IV);
-        
         $tokenDecrypted =  explode("--::--",$tokenDecrypted);
-        //return $tokenDecrypted;
         if(count($tokenDecrypted)!= 3 && $tokenDecrypted[2] != "tryThisMF)"){
             return array("status"=>"not a valid token");
         }else{
-            
-            return array("role"=>$tokenDecrypted[1],"ID"=>$tokenDecrypted[0]);
+            return array("status"=>"valid","role"=>$tokenDecrypted[1],"ID"=>$tokenDecrypted[0]);
         }
+        
     }
 
 
-    public function RBACencrypt_token_for($user,$password){
+    public function RBACmake_token_for(string $user,string $password){
         $role = "";
         $query = "select role from ".$this->loginTable." where ".$this->IDcollumn."= ? and ".$this->passwordCollumn." = ?";
         $stmt = $this->connection()->prepare($query);
@@ -76,7 +81,9 @@ class RBAC_TokenProcessor{
     }
 
     public function RBACisTokenAllowed(string $token, array $allowedRole){
-       
+       if($this->RBACgetTokenDetail($token)["status"] != "valid"){
+            return false;
+       }else{
         foreach($allowedRole as $ar){
             if($ar == $this->RBACgetTokenDetail($token)["role"]){
                 return true;
@@ -84,6 +91,8 @@ class RBAC_TokenProcessor{
                 return false;
             }
         }
+       }
+        
     }
 
 
